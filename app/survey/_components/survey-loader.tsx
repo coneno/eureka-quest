c8b47696e61d24f70f77292da8c4994c0d68ade3
phev8 <phev8@users.noreply.github.com>
2024-07-19 23:36:49 +0200

    generate uuid on page load for participant id

--- app/survey/_components/survey-loader.tsx
+++ app/survey/_components/survey-loader.tsx
@@ -1,10 +1,8 @@
 import Container from '@/components/container';
 import React from 'react';
-// import SurveyClient from './survey-client';
 import { Survey } from 'survey-engine/data_types';
-import { getLocaleStringTextByCode } from '@/components/survey-renderer/SurveySingleItemView/utils';
-//import { getLocalizedString } from '@/lib/get-localizes-string';
 import { promises as fs } from 'fs';
+import { v4 as uuidv4 } from 'uuid';
 
 import SurveyClient, { SurveyWithContext } from './survey-client';
 
@@ -13,24 +11,12 @@ interface SurveyLoaderProps {
 }
 
 const SurveyLoader: React.FC<SurveyLoaderProps> = async (props) => {
-    /*const t = await getTranslations({ locale: props.locale, namespace: 'SurveyPage' });
-
-    const resp = await getSurveyWithContextForProfile(props.studyKey, props.surveyKey, props.profileId);
-    if (!resp || resp.error || !resp.surveyWithContext) {
-        console.error(resp.error);
-        return <Container className='flex justify-center'>
-            <ErrorInfo
-                title={t('surveyLoadingError')}
-                description={resp?.error}
-            />
-        </Container>;
-    }
-
-    const survey = resp.surveyWithContext.survey as Survey;
-    const title = getLocalizedString(survey.props?.name, props.locale);*/
-
     const file = await fs.readFile(process.cwd() + '/public/TestSurvey.json', 'utf8');
     const survey = JSON.parse(file) as Survey;
+
+    // Generate a new UUID for the profileID
+    const participantID = uuidv4();
+
 
     const surveyWithContext: SurveyWithContext = {
         survey: survey,
@@ -61,7 +47,7 @@ const SurveyLoader: React.FC<SurveyLoaderProps> = async (props) => {
                         openAt={Math.floor(Date.now() / 1000)}
                         studyKey={'TestStudy'}
                         surveyKey={survey.surveyDefinition.key}
-                        profileID={'todo-participant-id'}
+                        profileID={participantID}
                     />
                 </div>
             </Container>

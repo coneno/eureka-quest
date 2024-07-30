58704ed5ca8d264442e74d793fa69f28af5bff52
phev8 <phev8@users.noreply.github.com>
2024-07-30 11:39:45 +0200

    use symbol on success page

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,3 +1,5 @@
+import { CheckCircle } from "lucide-react";
+
 export default function Page() {
 
     return (
@@ -8,7 +10,12 @@ export default function Page() {
             tabIndex={-1}
         >
             <div className="flex flex-col items-center justify-center max-w-md">
-                <h1 className="text-3xl font-bold mb-4">Success!</h1>
+
+                <h1 className="text-3xl font-bold mb-4 flex items-center justify-center gap-6">
+                    <div className="">
+                        <CheckCircle className='size-12 text-primary' />
+                    </div>
+                    Success!</h1>
                 <p className="text-lg">You have successfully completed the survey.</p>
                 <p className="text-lg">Thank you for your participation!</p>
             </div>
